16528376e2b13ef7a602f26da0b5b984ee0a527e
razuwa <zidniirajwa@gmail.com>
2025-11-18 23:37:28 +0700

    feat: menu kasir

--- Project/ManajemenStokdanKasir.java
+++ Project/ManajemenStokdanKasir.java
@@ -9,9 +9,9 @@ public class ManajemenStokdanKasir {
         ArrayList<String> kodeBarang = new ArrayList<>();
         ArrayList<String> namaBarang = new ArrayList<>();
         ArrayList<Double> daftarHarga = new ArrayList<>();
-        ArrayList<Double> daftarStok = new ArrayList<>();
+        ArrayList<Double> jumlahStok = new ArrayList<>();
         String namaKasir;
-        int i = 0, tanggalHari, nomorPelanggan;
+        int tanggalHari, nomorPelanggan;
         double totalBelanja = 0.0, pendapatanHarian = 0.0;
         char konfirmasi;
         char lanjut = 'y';
@@ -35,11 +35,11 @@ public class ManajemenStokdanKasir {
         System.out.println("Login Berhasil. Selamat Bekerja, " + namaKasir + "!");
 
         //Stok Awal Barang
-        kodeBarang.add("001"); namaBarang.add("Beras"); daftarHarga.add(15000.0); daftarStok.add(50.0);
-        kodeBarang.add("002"); namaBarang.add("Gula"); daftarHarga.add(12000.0); daftarStok.add(30.0);
-        kodeBarang.add("003"); namaBarang.add("Minyak Goreng"); daftarHarga.add(14000.0); daftarStok.add(20.0);
-        kodeBarang.add("004"); namaBarang.add("Mi Instan"); daftarHarga.add(4000.0); daftarStok.add(120.0);
-        kodeBarang.add("005"); namaBarang.add("Gas LPG"); daftarHarga.add(20000.0); daftarStok.add(10.0);
+        kodeBarang.add("001"); namaBarang.add("Beras"); daftarHarga.add(15000.0); jumlahStok.add(50.0);
+        kodeBarang.add("002"); namaBarang.add("Gula"); daftarHarga.add(12000.0); jumlahStok.add(30.0);
+        kodeBarang.add("003"); namaBarang.add("Minyak Goreng"); daftarHarga.add(14000.0); jumlahStok.add(20.0);
+        kodeBarang.add("004"); namaBarang.add("Mi Instan"); daftarHarga.add(4000.0); jumlahStok.add(120.0);
+        kodeBarang.add("005"); namaBarang.add("Gas LPG"); daftarHarga.add(20000.0); jumlahStok.add(10.0);
 
         //Menu Utama
         boolean bekerja = true;
@@ -49,15 +49,80 @@ public class ManajemenStokdanKasir {
             System.out.println("2. Kelola Stok Barang");
             System.out.println("3. Selesai bekerja");
             System.out.print("Pilih menu (1-3): ");
-            int pilihMenu = scanner.nextInt();
+            int pilihMenu = scanner.nextInt(); scanner.nextLine();
 
             switch (pilihMenu) {
+                // KASIR
                 case 1:
+                    System.out.println("\n=== Menu Kasir ===");
+                    //Buat arraylist baru untuk keranjang belanja
                     ArrayList<String> keranjangKode = new ArrayList<>();
                     ArrayList<String> keranjangNama = new ArrayList<>();
                     ArrayList<Integer> keranjangJumlah = new ArrayList<>();
                     ArrayList<Double> keranjangHargaSatuan = new ArrayList<>();
                     ArrayList<Double> keranjangSubtotal = new ArrayList<>();
+
+                    //Looping transaksi tiap satu barang
+                    while (true) {
+                        System.out.print("Masukkan Kode Barang (atau '0' untuk selesai): ");
+                        String kodeInput = scanner.nextLine();
+                        if (kodeInput.equals("0")) {
+                            break;
+                        }
+                        
+                        //Logika mencari barang dalam arraylist
+                        int indeksDitemukan = -1;
+                        for (int i = 0; i < kodeBarang.size(); i++) {
+                            if (kodeBarang.get(i).equals(kodeInput)) {
+                                indeksDitemukan = i;
+                                break;
+                            }
+                        }
+                        //Kalo barang ga ketemu
+                        if (indeksDitemukan == -1) {
+                            System.out.println("Kode barang tidak ditemukan");
+                            }
+
+                            //Kalo barang ketemu
+                        else{
+                            //variabel ini dah pokoknya, ngambil dari arraylist based on indeksDitemukan
+                            String namaBrg = namaBarang.get(indeksDitemukan);
+                            double hargaBrg = daftarHarga.get(indeksDitemukan);
+                            double stokBrg = jumlahStok.get(indeksDitemukan);
+
+                            //Logika jumlah beli
+                            int jumlahBeli = 0;
+                            while(true){
+                                System.out.print("Masukkan jumlah beli: ");
+                                jumlahBeli = scanner.nextInt(); scanner.nextLine();
+                                if (jumlahBeli <= 0) {
+                                    System.out.println("Jumlah beli tidak boleh 0 ");
+                                } else {
+                                    break;
+                                }
+                            }
+
+                            //Nambahin ke keranjang
+                            keranjangKode.add(kodeInput);
+                            keranjangNama.add(namaBrg);
+                            keranjangJumlah.add(jumlahBeli);
+                            keranjangHargaSatuan.add(hargaBrg);
+                            keranjangSubtotal.add(hargaBrg * jumlahBeli);
+
+                            //Kurangi stok total
+                            jumlahStok.set(indeksDitemukan, stokBrg - jumlahBeli);
+                            System.out.println(namaBrg + " sebanyak " + jumlahBeli + " Berhasil ditambahkan");
+                        }
+                    }
+                    
+                    break;
+                case 2:
+                    System.out.println("\n=== Kelola Stok Barang ===");
+                    break;
+                case 3:
+                    System.out.println("Selesai bekerja.");
+                    bekerja = false;
+                    break; 
             }
             
         } while (bekerja);
